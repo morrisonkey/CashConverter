146c370a0740552d8f68c323455a3d8ba8068777
keyanbagheri <keyanbagheri@gmail.com>
2017-10-03 18:42:54 +0800

    creating exchangerateconverter class with method convert which checks for the most recent date within requested date

---
+++ app/models/exchange_rate_converter.rb
@@ -0,0 +1,16 @@
+class ExchangeRateConverter
+
+
+  def self.convert(usd, date)
+    usd * get_most_recent_exchange(date).rate
+  end
+
+  def self.get_most_recent_exchange(date)
+    datetime = Date.parse(date)
+    until Exchange.find(datetime)
+      datetime -= 1.day
+    end
+    Exchange.find(datetime)
+  end
+
+end
