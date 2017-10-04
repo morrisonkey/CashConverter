90d9c4faf35788b0308232fcc252fcb0667b205a
keyanbagheri <keyanbagheri@gmail.com>
2017-10-04 10:05:02 +0800

    fixing the math on the equation

--- app/models/exchange_rate_converter.rb
+++ app/models/exchange_rate_converter.rb
@@ -1,7 +1,7 @@
 class ExchangeRateConverter
 
   def self.convert(usd, date)
-    usd * get_most_recent_exchange(date).rate
+    usd / get_most_recent_exchange(date).rate
   end
 
   def self.get_most_recent_exchange(date)
